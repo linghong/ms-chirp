b84bdf398e9bba5ff773d99efb886674d8e2ff80
linghong <linghong_chen@hotmail.com>
2015-06-09 13:30:54 -0400

    added index login and register pages

--- app/js/chirpApp.js
+++ app/js/chirpApp.js
@@ -1,7 +1,27 @@
 //set up module called chirpApp 
-var app = angular.module('chirpApp', []);
+var app = angular.module('chirpApp', ['ngRoute']);
 
-//set up module controller
+app.config(function($routeProvider){
+  $routeProvider
+    //the timeline display
+    .when('/', {
+      templateUrl: 'main.html',
+      controller: 'mainController'
+    })
+    //the login display
+    .when('/login', {
+      templateUrl: 'login.html',
+      controller: 'authController'
+    })
+    //the signup display
+    .when('/register', {
+      templateUrl: 'register.html',
+      controller: 'authController'
+    });
+});
+
+
+//set up module maincontroller
 app.controller('mainController', function($scope){
   $scope.posts = [];
   $scope.newPost = {created_by: '', text: '', created_at: ''};
@@ -10,5 +30,21 @@ app.controller('mainController', function($scope){
 	$scope.newPost.created_at = Date.now();
 	$scope.posts.push($scope.newPost);
 	$scope.newPost = {created_by: '', text: '', created_at: ''};
+  };
+});
+
+//set up module authcontroller
+app.controller('authController', function($scope){
+  $scope.user = {username: '', password: ''};
+  $scope.error_message = '';
+
+  $scope.login = function(){
+    //placeholder until authentication is implemented
+    $scope.error_message = 'login request for ' + $scope.user.username;
+  };
+
+  $scope.register = function(){
+    //placeholder until authentication is implemented
+    $scope.error_message = 'registeration request for ' + $scope.user.username;
   };
 });
